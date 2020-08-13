6852595fead1ae638fddb6d65c474d1ed5b72927
dushitaoyuan <dushitaoyuan@gmail.com>
2020-08-13 23:50:32 +0800

    fix filebeat logstash conf

--- docker-compose-demo/docker-jenkins-demo/files/shell/jenkins_deploy.sh
+++ docker-compose-demo/docker-jenkins-demo/files/shell/jenkins_deploy.sh
@@ -6,7 +6,7 @@ app_version="v1"
 app_env="prod"
 app_name="docker-jenkins-demo"
 image_tag="$app_env"
-image_name="$docker_regist_repo/demo/$app_name_$app_version:$image_tag"
+image_name="$docker_regist_repo/$app_name_$app_version:$image_tag"
 #docker 集群ip
 docker_node_ip="192.168.30.150 192.168.30.153"
 
@@ -27,23 +27,25 @@ docker rmi $image_name
 
 # docker deploy prepare
 
-sudo dos2unix files/shell/*sh
+su root
+dos2unix files/shell/*sh
+
 for node_ip in ${docker_node_ip}
 do
-echo `id`
-echo $node_ip
-sudo ssh root@$node_ip  "mkdir -p /home/deploy/shell"
-sudo scp files/shell/docker-volume.sh root@$node_ip:/home/deploy/shell/
-sudo ssh root@$node_ip  "cd /home/deploy/shell;sh docker-volume.sh create"
-sudo scp -r files/mysql/* root@$node_ip:/home/mysql
-sudo scp -r files/nginx/* root@$node_ip:/home/nginx
+sudo -s ssh root@$node_ip  "mkdir -p /home/deploy/shell"
+sudo -s scp files/shell/docker-volume.sh root@$node_ip:/home/deploy/shell/
+sudo -s ssh root@$node_ip  "cd /home/deploy/shell;sh docker-volume.sh create"
+sudo -s scp -r files/mysql/* root@$node_ip:/home/mysql
+sudo -s scp -r files/nginx/* root@$node_ip:/home/nginx
 done
 
 
 # docker deploy
+
 docker stack rm $app_name
 docker stack deploy $app_name -c=docker-stack.yml
 docker stack ps $app_name
+
 
 
 
@@ -55,3 +57,6 @@ docker stack ps $app_name
 ## 将jenkins 加入docker组
 #gpasswd -a jenkins docker
 #newgrp docker
+
+# 支持 sudo  vim /etc/sudoers
+# jenkins ALL = NOPASSWD: ALL
